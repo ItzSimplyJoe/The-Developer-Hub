19ea7364f1af0b33d0b5fe9daca299d4efc5fe32
Joe Bostock <joebostock30@gmail.com>
2024-06-01 19:03:15 +0100

    Started Account page

---
+++ LearnToCode/Components/Pages/Account/AccountEnums/Language.cs
@@ -0,0 +1,33 @@
+﻿namespace DeveloperHub.Components.Pages.Account.AccountEnums
+{
+    public enum Language
+    {
+        English = 0,
+        Spanish = 1,
+        Mandarin = 2,
+        Hindi = 3,
+        Arabic = 4,
+        Bengali = 5,
+        Portuguese = 6,
+        Russian = 7,
+        Japanese = 8,
+        German = 9,
+        French = 10,
+        Urdu = 11,
+        Swahili = 12,
+        Korean = 13,
+        Italian = 14,
+        Turkish = 15,
+        Dutch = 16,
+        Thai = 17,
+        Vietnamese = 18,
+        Polish = 19,
+        Malay = 20,
+        Persian = 21,
+        Ukrainian = 22,
+        Filipino = 23,
+        Romanian = 24,
+        Indonesian = 25
+    }
+
+}
---
+++ LearnToCode/Components/Pages/Account/AccountEnums/Timezone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeveloperHub.Components.Pages.Account.AccountEnums
+{
+    public enum TimeZone
+    {
+        Utc = 0,
+        Est = 1,
+        Cst = 2,
+        Mst = 3,
+        Pst = 4,
+        Akst = 5,
+        Hst = 6,
+        Acst = 7,
+        Aest = 8,
+        Jst = 9,
+        Ist = 10,
+        Nst = 11,
+        Nzst = 12,
+        Sgt = 13,
+        Cet = 14,
+        Eet = 15,
+        Msk = 16,
+        Bst = 17,
+        Gst = 18,
+        Art = 19,
+        Brst = 20,
+        Clt = 21,
+        Ect = 22,
+        Uyt = 23,
+        Azost = 24,
+        Wet = 25,
+        West = 26
+    }
+}
